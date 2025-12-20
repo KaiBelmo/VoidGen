06e8ac32eb53ad569786c346e2710b05a6aa9233
KaiBelmo <belmomohamedali@gmail.com>
2025-12-21 02:06:10 +0700

    refactor(server): pass file path to Server constructor

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { runCli } from './cli/cli';
 export const startApp = ({ file, port }: CliArgs) => {
   console.log(`Starting with file: ${file} on port: ${port}`);
   const dataSource = new JsonDataSource(file);
-  const server = new Server(dataSource, port);
+  const server = new Server(dataSource, port, file);
   server.start();
 };
 
