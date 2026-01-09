8ae6fb25e6eb99c7a2f30ae80c1bfbdb4c72e431
KaiBelmo <belmomohamedali@gmail.com>
2026-01-09 10:33:46 +0100

    test: configure module name mapping for path aliases in Jest

--- jest.config.cjs
+++ jest.config.cjs
@@ -4,6 +4,9 @@ module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.test.ts'],
   moduleFileExtensions: ['ts', 'js', 'json'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
